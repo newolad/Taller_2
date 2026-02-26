0e23f29eaf7f61df23631f2fc563aaf9382614c4
newolad <newolad.development@gmail.com>
2026-02-25 23:38:31 -0500

    Inicio de proyecto

---
+++ app/src/main/java/com/example/clase_dos/activities/OnboardingActivity.kt
@@ -0,0 +1,30 @@
+package com.example.clase_dos.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.clase_dos.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_onboarding)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val botonComienza = findViewById<Button>(R.id.BotonComienza)
+        botonComienza.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
