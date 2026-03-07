0a40dc7713a94118e2d9b1bd36772f7e8cfca65f
newolad <newolad.development@gmail.com>
2026-03-06 19:34:06 -0500

    Ajuste con LinearLayout y activity register

--- app/src/main/java/com/example/clase_dos/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/clase_dos/activities/RegisterActivity.kt
@@ -9,21 +9,23 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.clase_dos.R
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val tvRegistrate = findViewById<TextView>(R.id.Registrate)
-        tvRegistrate.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
+        val tvIrLogin = findViewById<TextView>(R.id.tv_ir_login)
+        tvIrLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
